831f825b1b6f42888f698450c26814fba14d7007
Raymond Neilson <rneilson@interloper.ca>
2019-04-14 18:22:27 -0400

    make_context_config(); return ctx, path from get_context_config()

--- darkwing/config/context.py
+++ darkwing/config/context.py
@@ -10,13 +10,49 @@ def get_context_config(name='default', dirs=None, rootless=None, uid=None):
         rootless = not probably_root()
 
     if dirs is None:
-        curr_cfg = Path.cwd() / '.darkwing'
-        base_cfg, _, _ = default_base_paths(rootless=rootless, uid=uid)
-        dirs = [curr_cfg, base_cfg]
+        cwd_base = Path.cwd() / '.darkwing'
+        cfg_base, _, _ = default_base_paths(rootless=rootless, uid=uid)
+        dirs = [cwd_base, cfg_base]
 
     for dirp in dirs:
-        cfgp = (Path(dirp) / name).with_suffix('.toml')
-        if cfgp.exists():
-            return toml.load(cfgp)
+        ctx_path = (Path(dirp) / name).with_suffix('.toml')
+        if ctx_path.exists():
+            return toml.load(ctx_path), ctx_path
 
-    return None
+    return None, None
+
+def make_context_config(name='default', rootless=None, uid=None, gid=None):
+    if rootless is None:
+        rootless = not probably_root()
+
+    euid = os.geteuid()
+    egid = os.getegid()
+
+    if uid is None:
+        uid = euid
+    if gid is None:
+        gid = egid
+
+    cfg_base, sto_base, _ = default_base_paths(rootless=rootless, uid=uid)
+    ctx_path = (Path(cfg_base) / name).with_suffix('.toml')
+    do_chown = uid != euid or gid != egid
+
+    # Create any parent dir(s)
+    if not cfg_base.exists():
+        cfg_base.mkdir(mode=0o775, parents=True, exist_ok=True)
+        if do_chown:
+            os.chown(cfg_base, uid, gid)
+    if not sto_base.exists():
+        sto_base.mkdir(mode=0o775, parents=True, exist_ok=True)
+        if do_chown:
+            os.chown(sto_base, uid, gid)
+
+    # Touch mostly to raise FileExistsError
+    ctx_path.touch(mode=0o664, exist_ok=False)
+    if do_chown:
+        os.chown(ctx_path, uid, gid)
+
+    context = default_context(name, rootless=rootless, uid=uid, gid=gid)
+    ctx_path.write_text(toml.dumps(context))
+
+    return context, ctx_path
