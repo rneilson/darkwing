6f9236fb695361aff1ae2abd67091b939ca50b2f
Raymond Neilson <rneilson@interloper.ca>
2019-04-22 15:13:50 -0400

    Implement set_subreaper()

--- darkwing/utils/syscalls.py
+++ darkwing/utils/syscalls.py
@@ -3,8 +3,15 @@ import ctypes.util
 import errno
 import os
 
-def set_subreaper():
-    raise NotImplementedError
+_libc = ctypes.CDLL(ctypes.util.find_library('c'), use_errno=True)
+
+PR_SET_PDEATHSIG = 1
+PR_SET_NAME = 15
+PR_SET_CHILD_SUBREAPER = 36
+
+def set_subreaper(target=True):
+    arg2 = 1 if target else 0
+    _libc.prctl(PR_SET_CHILD_SUBREAPER, arg2, 0, 0, 0)
 
 def unshare_namespaces():
     raise NotImplementedError
